9775d5d33275b69d85f048afba455fbe503dae6d
Luis Adolfo Echeverri Molina <130845740+Ocre2@users.noreply.github.com>
2023-09-14 19:15:07 -0500

    Add files via upload

---
+++ Examen_Tren/Examen_Tren/TrenDePasajeros.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Tren
+{
+    class TrenDePasajeros : TrenBase
+    {
+        private int estacionesRecorridas;
+        private int pasajerosTransportados;
+
+        public TrenDePasajeros(string nombre, int capacidadPasajeros, int estaciones)
+            : base(nombre, 0)
+        {
+            this.estacionesRecorridas = 0;
+            this.pasajerosTransportados = 0;
+            this.CapacidadPasajeros = capacidadPasajeros;
+            this.Estaciones = estaciones;
+        }
+
+        public int CapacidadPasajeros { get; private set; }
+        public int Estaciones { get; private set; }
+
+        public override int CapacidadMaxima()
+        {
+            return CapacidadPasajeros;
+        }
+
+        public void RegistrarArriboEstacion()
+        {
+            estacionesRecorridas++;
+            pasajerosTransportados += pasajeros_abordo;
+            ArribarEstacion();
+        }
+
+        public double PasajerosPorEstacion()
+        {
+            if (estacionesRecorridas == 0)
+            {
+                return 0;
+            }
+            return (double)pasajerosTransportados / estacionesRecorridas;
+        }
+    }
+}
